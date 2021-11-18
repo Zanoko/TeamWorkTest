813298b32671193c7700d0d09cb535f788596b33
Ruoyu <xyhuyumao@qq.com>
2021-11-18 15:17:39 +0900

    Share An AppBuild
    update

---
+++ ForGitHubSample/ForGitHubSample/ForGitHubSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForGitHubSampleApp.swift
+//  ForGitHubSample
+//
+//  Created by cmStudent on 2021/11/18.
+//
+
+import SwiftUI
+
+@main
+struct ForGitHubSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
